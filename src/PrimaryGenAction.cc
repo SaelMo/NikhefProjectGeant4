63aa53300cc13bb8ae346e472eb578b90ddce805
Saad el Morabit <morabitelsaad@gmail.com>
2025-04-03 18:22:45 +0200

    Update PrimaryGenAction.cc

--- src/PrimaryGenAction.cc
+++ src/PrimaryGenAction.cc
@@ -2,21 +2,5 @@
 #include "G4ParticleTable.hh"
 #include "G4SystemOfUnits.hh"
 #include "G4GeneralParticleSource.hh"
-#include "Randomize.hh" // Include for random number generation
 
-namespace NikhefProject {
-
-PrimaryGenAction::PrimaryGenAction(){
-  G4double particleEnergy = 2000 * keV;
-  G4ThreeVector particlePosition = G4ThreeVector(0.1 * m, 0.1 * m, 0.1 * m);
-
-  fParticleGun = new G4GeneralParticleSource();
-  fParticleGun->SetParticleDefinition(G4ParticleTable::GetParticleTable()->FindParticle("gamma")); 
-
-  auto energyDist = fParticleGun->GetCurrentSource()->GetEneDist();
-  energyDist->SetMonoEnergy(particleEnergy);
-
-  fParticleGun->SetParticlePosition(particlePosition);
-}
-
-}
+namespace NikhefProject {}
