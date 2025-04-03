a031b2e7ff589afce1879afe7a5ef2ea0249e245
Saad el Morabit <morabitelsaad@gmail.com>
2025-04-03 18:28:47 +0200

    Update PrimaryGenAction.hh

--- include/PrimaryGenAction.hh
+++ include/PrimaryGenAction.hh
@@ -1,18 +1,1 @@
-#include "G4VUserPrimaryGeneratorAction.hh"
-#include "G4ParticleGun.hh"
-#include "G4ios.hh"
-#include "G4GeneralParticleSource.hh"
 
-namespace NikhefProject
-{
-
-class PrimaryGenAction : public G4VUserPrimaryGeneratorAction {
-public:
-  PrimaryGenAction();
-  void GeneratePrimaries(G4Event* anEvent) override;
-
-private:
-  G4GeneralParticleSource* fParticleGun;
-};
-  
-}
