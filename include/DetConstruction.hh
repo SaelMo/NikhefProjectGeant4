1c7144cbfaf3260f101c479e472c6366b9acdae6
Saad el Morabit <morabitelsaad@gmail.com>
2025-04-03 18:28:35 +0200

    Update DetConstruction.hh

--- include/DetConstruction.hh
+++ include/DetConstruction.hh
@@ -1,10 +1,1 @@
-#include "G4VUserDetectorConstruction.hh"
 
-namespace NikhefProject {
-
-class DetConstruction : public G4VUserDetectorConstruction {
-public:
-  G4VPhysicalVolume* Construct();
-};
-
-}
