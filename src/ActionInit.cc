08db6942ca535834124af8b903653a88cc85cfbd
Saad el Morabit <70237218+SaelMo@users.noreply.github.com>
2025-04-03 18:41:04 +0200

    update

--- src/ActionInit.cc
+++
@@ -1,8 +0,0 @@
-#include "ActionInit.hh"
-#include "PrimaryGenAction.hh"
-#include "SteppingAction.hh"
-
-void ActionInit::Build() const {
-  SetUserAction(new PrimaryGenAction());
-  SetUserAction(new SteppingAction());
-}
