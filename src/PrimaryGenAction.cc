0c7d1410041ec91b069fb5ddf0a3a3bb551b44a6
Saad el Morabit <morabitelsaad@gmail.com>
2025-04-03 18:24:58 +0200

    Update PrimaryGenAction.cc

--- src/PrimaryGenAction.cc
+++ src/PrimaryGenAction.cc
@@ -3,4 +3,13 @@
 #include "G4SystemOfUnits.hh"
 #include "G4GeneralParticleSource.hh"
 
-namespace NikhefProject {}
+// For starters, we shoot a geantino (interactionless test particle) with one MeV of energy from the origin
+PrimaryGenAction::PrimaryGenAction(){
+  G4double particleEnergy = 1000 * keV;
+  G4ThreeVector particlePosition = G4ThreeVector(0.0 * m, 0.0 * m, 0.0 * m);
+  fParticleGun = new G4GeneralParticleSource();
+  fParticleGun->SetParticleDefinition(G4ParticleTable::GetParticleTable()->FindParticle("geantino")); 
+  auto energyDist = fParticleGun->GetCurrentSource()->GetEneDist();
+  energyDist->SetMonoEnergy(particleEnergy);
+  fParticleGun->SetParticlePosition(particlePosition);
+}
