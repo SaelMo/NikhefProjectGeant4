53f267ed7adc66ecd2b3e9d70e997b5fa2dc5561
Saad el Morabit <morabitelsaad@gmail.com>
2025-04-03 18:28:22 +0200

    Update ActionInit.hh

--- include/ActionInit.hh
+++ include/ActionInit.hh
@@ -1,11 +1,1 @@
-#pragma once
 
-#include "G4VUserActionInitialization.hh"
-
-namespace NikhefProject {
-
-class ActionInit : public G4VUserActionInitialization {
-public:
-  void Build() const override;
-  };
-}
