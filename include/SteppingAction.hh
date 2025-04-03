3936e5cde616e0bbdf8992ccf287b71e4ca88698
Saad el Morabit <morabitelsaad@gmail.com>
2025-04-03 18:28:57 +0200

    Update SteppingAction.hh

--- include/SteppingAction.hh
+++ include/SteppingAction.hh
@@ -1,10 +1,1 @@
-#include "G4UserSteppingAction.hh"
 
-namespace NikhefProject {
-
-class SteppingAction : public G4UserSteppingAction {
-public:
-  void UserSteppingAction(const G4Step*) override;
-};
-
-}
